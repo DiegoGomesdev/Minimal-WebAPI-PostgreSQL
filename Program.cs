9a44981e10aa443e9988949d984fb2454cc67263
Diego Gomes <digomesrique@hotmail.com>
2022-07-07 15:27:52 -0300

    Finalizaçao do projeto

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ app.MapPost("AdicionarModelosEquipamento", async (Equipment_model equipment_mode
 {
     contexto.Equipment_model.Add(equipment_model);
     await contexto.SaveChangesAsync();
-}).WithTags("Modelos Equipamentos");
+}).WithTags("Modelo dos Equipamentos");
 
 app.MapDelete("ExcluirModeloEquipamento/{id}", async (Guid id, Contexto contexto) =>
 {
@@ -78,19 +78,19 @@ app.MapDelete("ExcluirModeloEquipamento/{id}", async (Guid id, Contexto contexto
         contexto.Equipment_model.Remove(modeloEquipamentoExcluir);
         await contexto.SaveChangesAsync();
     }
-}).WithTags("Modelos Equipamentos");
+}).WithTags("Modelo dos Equipamentos");
 
 app.MapGet("ListarModelosEquipamentos", async (Contexto contexto) =>
 {
     return await contexto.Equipment_model.ToListAsync();
-}).WithTags("Modelos Equipamentos");
+}).WithTags("Modelo dos Equipamentos");
 
 app.MapGet("PesquisarModeloEquipamento/{id}", async (Guid id, Contexto contexto) =>
 
     await contexto.Equipment_model.FindAsync(id) is Equipment_model equipmentModel
     ? Results.Ok(equipmentModel)
     : Results.NotFound())
-    .WithTags("Modelos Equipamentos");
+    .WithTags("Modelo dos Equipamentos");
 
 app.MapPut("AtualizarModeloEquipamento/{id}", async (Guid id, Contexto contexto) =>
 {
@@ -100,17 +100,17 @@ app.MapPut("AtualizarModeloEquipamento/{id}", async (Guid id, Contexto contexto)
         contexto.Equipment_model.Update(modeloEquipamentoAtualizar);
         await contexto.SaveChangesAsync();
     }
-}).WithTags("Modelos Equipamentos");
+}).WithTags("Modelo dos Equipamentos");
 #endregion
 
 #region Equipment_model_state_hourly_earnings Actions
-app.MapPost("Adicionar EquipmentModel_SHE ", async (Equipment_model_state_hourly_earnings equipment_mshe, Contexto contexto) =>
+app.MapPost("Adicionar EquipmentModel_SHE", async (Equipment_model_state_hourly_earnings equipment_mshe, Contexto contexto) =>
 {
     contexto.Equipment_model_state_hourly_earnings.Add(equipment_mshe);
     await contexto.SaveChangesAsync();
     return Results.Created($"/Equipamento Adicionado/{equipment_mshe.Equipment_model_id}", equipment_mshe);
 
-}) .WithTags("Equipment_model_state_hourly_earnings");
+}) .WithTags("Ganhos por Hora/Equipamentos");
 
 app.MapDelete("Excluir EquipmentModel_SHE/{id}", async (Guid id, Contexto contexto) =>
 {
@@ -120,20 +120,20 @@ app.MapDelete("Excluir EquipmentModel_SHE/{id}", async (Guid id, Contexto contex
         contexto.Equipment_model_state_hourly_earnings.Remove(equipModelsheExcluir);
         await contexto.SaveChangesAsync();
     }
-}).WithTags("Equipment_model_state_hourly_earnings");
+}).WithTags("Ganhos por Hora/Equipamentos");
 
 app.MapGet("Listar EquipmentModel_SHE", async (Contexto contexto) => 
 { 
     return await contexto.Equipment_model_state_hourly_earnings.ToListAsync(); 
 })
-   .WithTags("Equipment_model_state_hourly_earnings");
+   .WithTags("Ganhos por Hora/Equipamentos");
 
 app.MapGet("Pesquisar EquipmentModel_SHE/{id}", async (Guid id, Contexto contexto) =>
 
-    await contexto.Equipment_model_state_hourly_earnings.FindAsync(id) is Equipment_model_state_hourly_earnings equipment_mshe
+    await contexto.Equipment_model_state_hourly_earnings.Where(e => e.Equipment_model_id == id).ToListAsync() is List<Equipment_model_state_hourly_earnings> equipment_mshe
     ? Results.Ok(equipment_mshe)
     : Results.NotFound("Resultado nao encontrado !!"))
-    .WithTags("Equipment_model_state_hourly_earnings");
+    .WithTags("Ganhos por Hora/Equipamentos");
 
 app.MapPut("Atualizar EquipmentModel_SHE/{id}", async (Guid id, Equipment_model_state_hourly_earnings inputEquip, Contexto contexto) =>
 {
@@ -146,17 +146,17 @@ app.MapPut("Atualizar EquipmentModel_SHE/{id}", async (Guid id, Equipment_model_
     return Results.NoContent()
     ;
 })
-    .WithTags("Equipment_model_state_hourly_earnings");
+    .WithTags("Ganhos por Hora/Equipamentos");
 #endregion
 
 #region Equipment Position History Actions
-app.MapPost("Adicionar Equipment Position History ", async (Equipment_position_history equipment_position, Contexto contexto) =>
+app.MapPost("Adicionar Equipment Position History", async (Equipment_position_history equipment_position, Contexto contexto) =>
 {
     contexto.Equipment_position_history.Add(equipment_position);
     await contexto.SaveChangesAsync();
     return Results.Created($"/Equipamento Adicionado/{equipment_position.Equipment_id}", equipment_position);
 
-}).WithTags("Equipment Position History");
+}).WithTags("Historico de Posiçao Equipamentos");
 
 app.MapDelete("Excluir Equipment Position History/{id}", async (Guid id, Contexto contexto) =>
 {
@@ -166,19 +166,19 @@ app.MapDelete("Excluir Equipment Position History/{id}", async (Guid id, Context
         contexto.Equipment_position_history.Remove(equipPositionExcluir);
         await contexto.SaveChangesAsync();
     }
-}).WithTags("Equipment Position History");
+}).WithTags("Historico de Posiçao Equipamentos");
 
 app.MapGet("Listar Equipment Position History", async (Contexto contexto) =>
 
     await contexto.Equipment_position_history.ToListAsync())
-   .WithTags("Equipment Position History");
+   .WithTags("Historico de Posiçao Equipamentos");
 
 app.MapGet("Pesquisar Equipment Position History/{id}", async (Guid id, Contexto contexto) =>
 
-    await contexto.Equipment_position_history.FindAsync(id) is Equipment_position_history equipment_position
+    await contexto.Equipment_position_history.Where(e => e.Equipment_id == id).ToListAsync() is List<Equipment_position_history> equipment_position
     ? Results.Ok(equipment_position)
     : Results.NotFound("Resultado nao encontrado !!"))
-    .WithTags("Equipment Position History");
+    .WithTags("Historico de Posiçao Equipamentos");
 
 app.MapPut("Atualizar Equipment Position History/{id}", async (Guid id, Equipment_position_history inputEquip, Contexto contexto) =>
 {
@@ -191,7 +191,99 @@ app.MapPut("Atualizar Equipment Position History/{id}", async (Guid id, Equipmen
     return Results.NoContent()
     ;
 })
-    .WithTags("Equipment Position History");
+    .WithTags("Historico de Posiçao Equipamentos");
 #endregion
+
+#region Equipment_state Actions
+app.MapPost("Adicionar Estado do Equipamento", async (Equipment_state equipment_state, Contexto contexto) =>
+{
+    contexto.Equipment_state.Add(equipment_state);
+    await contexto.SaveChangesAsync();
+    return Results.Created($"/Adicionado/{equipment_state.Id}", equipment_state);
+
+}).WithTags("Estado do Equipamento");
+
+app.MapDelete("Excluir Estado do Equipamento/{id}", async (Guid id, Contexto contexto) =>
+{
+    var equipStateExcluir = await contexto.Equipment_state.FirstOrDefaultAsync(e => e.Id == id);
+    if (equipStateExcluir != null)
+    {
+        contexto.Equipment_state.Remove(equipStateExcluir);
+        await contexto.SaveChangesAsync();
+    }
+}).WithTags("Estado do Equipamento");
+
+app.MapGet("Listar Estado do Equipamento", async (Contexto contexto) =>
+
+    await contexto.Equipment_state.ToListAsync())
+   .WithTags("Estado do Equipamento");
+
+app.MapGet("Pesquisar Estado do Equipamento/{id}", async (Guid id, Contexto contexto) =>
+
+    await contexto.Equipment_state.Where(e => e.Id == id).ToListAsync() is List<Equipment_state> equipment_state
+    ? Results.Ok(equipment_state)
+    : Results.NotFound("Resultado nao encontrado !!"))
+    .WithTags("Estado do Equipamento");
+
+app.MapPut("Atualizar Estado do Equipamento/{id}", async (Guid id, Equipment_state inputEquip, Contexto contexto) =>
+{
+    var equipStateAtualizar = await contexto.Equipment_state.FindAsync(id);
+    if (equipStateAtualizar is null) return Results.NotFound("Resultado nao encontrado !!");
+    //AtualizarequipPosition.Value = inputEquip.Value;
+
+    await contexto.SaveChangesAsync();
+
+    return Results.NoContent()
+    ;
+})
+    .WithTags("Estado do Equipamento");
+#endregion
+
+#region Equipment_state_history Actions
+app.MapPost("Adicionar Historico de Estado do Equipamento", async (Equipment_state_history equipment_state_history, Contexto contexto) =>
+{
+    contexto.Equipment_state_history.Add(equipment_state_history);
+    await contexto.SaveChangesAsync();
+    return Results.Created($"/Adicionado/{equipment_state_history.Equipment_id}", equipment_state_history);
+
+}).WithTags("Historico de Estado do Equipamento");
+
+app.MapDelete("Excluir Historico de Estado do Equipamento/{id}", async (Guid id, Contexto contexto) =>
+{
+    var equipStateHistoryExcluir = await contexto.Equipment_state_history.FirstOrDefaultAsync(e => e.Equipment_id == id);
+    if (equipStateHistoryExcluir != null)
+    {
+        contexto.Equipment_state_history.Remove(equipStateHistoryExcluir);
+        await contexto.SaveChangesAsync();
+    }
+}).WithTags("Historico de Estado do Equipamento");
+
+app.MapGet("Listar Historico de Estado do Equipamento", async (Contexto contexto) =>
+
+    await contexto.Equipment_state_history.ToListAsync())
+   .WithTags("Historico de Estado do Equipamento");
+
+app.MapGet("Pesquisar Historico de Estado do Equipamento/{id}", async (Guid id, Contexto contexto) =>
+
+    await contexto.Equipment_state_history.Where(e => e.Equipment_id == id).ToListAsync() is List<Equipment_state_history> equipment_state_history
+    ? Results.Ok(equipment_state_history)
+    : Results.NotFound("Resultado nao encontrado !!"))
+    .WithTags("Historico de Estado do Equipamento");
+
+app.MapPut("Atualizar Historico de Estado do Equipamento/{id}", async (Guid id, Equipment_state_history inputEquip, Contexto contexto) =>
+{
+    var equipStateAtualizar = await contexto.Equipment_state_history.FindAsync(id);
+    if (equipStateAtualizar is null) return Results.NotFound("Resultado nao encontrado !!");
+    //AtualizarequipPosition.Value = inputEquip.Value;
+
+    await contexto.SaveChangesAsync();
+
+    return Results.NoContent()
+    ;
+})
+    .WithTags("Historico de Estado do Equipamento");
+#endregion
+
+
 
 app.Run();
